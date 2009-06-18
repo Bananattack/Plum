e96cdc148c82a3253a1eb9d6b26695369ed36ea0
Andrew Crowell <overkill9999@gmail.com>
2009-06-18 18:07:24 -0400

    Modified the system.lua slightly for the hell of it.

--- test/system.lua
+++ test/system.lua
@@ -6,12 +6,10 @@ fnt:enableVariableWidth()
 print("width = " .. tex.width .. "; height = " .. tex.height)
 
 time = plum.timer.time
+
 while true do
     plum.video.solidRect(0, 0, plum.video.screenWidth, plum.video.screenHeight, plum.color.rgb(0x33, 0x66, 0xcc))
-    
-    plum.video.opacity = math.sin(math.rad(plum.timer.time)) * 63 + 127
-    tex:blit(50, 50)
-    plum.video.opacity = 255
+    tex:blit(i, i)
     
     fnt:print(5, 5, "FPS: " .. plum.timer.fps);
     
