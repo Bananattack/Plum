1939b0ffa3c43729dc6854087e66a6ccc90acb41
Andrew Crowell <overkill9999@gmail.com>
2009-06-19 03:20:46 -0400

    Added support for all keyboard keys.

--- test/system.lua
+++ test/system.lua
@@ -7,12 +7,26 @@ print("width = " .. tex.width .. "; height = " .. tex.height)
 
 time = plum.timer.time
 
-while true do
+x = 0; y = 0;
+
+while not plum.key.Escape.pressed do
     plum.video.verticalGradientRect(0, 0, plum.video.screenWidth, plum.video.screenHeight, plum.color.rgb(0x33, 0x66, 0xcc), plum.color.Magenta)
-    tex:blit(50, 50)
+    tex:blit(x, y)
     
     fnt:print(5, 5, "FPS: " .. plum.timer.fps .. " " .. tostring(plum.key.Enter.pressed))
-    
+    for i = 1, plum.timer.gap do
+        if plum.key.Left.pressed then
+            x = x - 1
+        elseif plum.key.Right.pressed then
+            x = x + 1
+        end
+        
+        if plum.key.Up.pressed then
+            y = y - 1
+        elseif plum.key.Down.pressed then
+            y = y + 1
+        end
+    end
     plum.refresh()
 end
 
