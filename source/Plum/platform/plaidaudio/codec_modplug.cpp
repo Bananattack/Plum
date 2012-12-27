a0219f795cb971ef01e2341349a5acefaadb1fca
Andrew G. Crowell <overkill9999@gmail.com>
2012-12-27 02:26:27 -0500

    More nitpicking.

--- source/Plum/platform/plaidaudio/codec_modplug.cpp
+++ source/Plum/platform/plaidaudio/codec_modplug.cpp
@@ -11,195 +11,190 @@
 
 #include "../../core/file.h"
 
-namespace plaidgadget
+
+namespace
 {
-    namespace codec_modplug
+    class ModplugStream : public plaidgadget::AudioStream
     {
-        class ModplugStream : public AudioStream
-        {
-            public:
-                ModplugStream(String fn, bool loop)
-                    : loop(loop), finished(true), valid(false)
+        public:
+            ModplugStream(plaidgadget::String fn, bool loop)
+                : loop(loop), finished(true), valid(false)
+            {
                 {
-                    {
-                        std::unique_ptr<plum::File> file(new plum::File(ToStdString(fn), plum::FileRead));
-                        // Get current position.
-                        unsigned int pos = file->tell();
-                        // Length to read = position of end - current.
-                        unsigned int length = 0;
-                        file->seek(pos, plum::SeekEnd);
-                        length = file->tell() - pos;
-                        file->seek(pos, plum::SeekStart);
-                        // Read fully.
-                        std::vector<uint8_t> data(length);
-                        file->readRaw(data.data(), data.size());
-                        // Load the mod.
-                        mod = ModPlug_Load(data.data(), data.size());
-                    }
-                    if(mod)
-                    {
-                        // Info
-                        ModPlug_Settings settings;
-                        ModPlug_GetSettings(&settings);
-                        output.channels = settings.mChannels;
-                        output.rate = settings.mFrequency;
+                    std::unique_ptr<plum::File> file(new plum::File(plaidgadget::ToStdString(fn), plum::FileRead));
+                    // Get current position.
+                    unsigned int pos = file->tell();
+                    // Length to read = position of end - current.
+                    unsigned int length = 0;
+                    file->seek(pos, plum::SeekEnd);
+                    length = file->tell() - pos;
+                    file->seek(pos, plum::SeekStart);
+                    // Read fully.
+                    std::vector<uint8_t> data(length);
+                    file->readRaw(data.data(), data.size());
+                    // Load the mod.
+                    mod = ModPlug_Load(data.data(), data.size());
+                }
+                if(mod)
+                {
+                    // Info
+                    ModPlug_Settings settings;
+                    ModPlug_GetSettings(&settings);
+                    output.channels = settings.mChannels;
+                    output.rate = settings.mFrequency;
 
-                        valid = true;
-                        finished = false;
-                    }
+                    valid = true;
+                    finished = false;
+                }
+            }
+
+            virtual ~ModplugStream()
+            {
+                if(valid)
+                {
+                    ModPlug_Unload(mod);
+                }
+            }
+
+            bool success()
+            {
+                return valid;
+            }
+
+            virtual plaidgadget::AudioFormat format()
+            {
+                return output;
+            }
+
+            virtual bool exhausted()
+            {
+                return finished;
+            }
+
+            virtual void tick(plaidgadget::Uint64 frame)
+            {
+            }
+
+            virtual void pull(plaidgadget::AudioChunk& chunk)
+            {
+                if(!chunk.length())
+                {
+                    return;
                 }
 
-                virtual ~ModplugStream()
+                if(!valid || finished)
                 {
-                    if(valid)
-                    {
-                        ModPlug_Unload(mod);
-                    }
+                    chunk.silence();
+                    return;
                 }
 
-                bool success()
+                int have = 0;
+                int need = chunk.length();
+
+                // Expand the intermediate buffer.
+                if(need * chunk.format().channels > buffer.size())
                 {
-                    return valid;
+                    buffer.resize(need * chunk.format().channels);
                 }
 
-                virtual AudioFormat format()
+                // Create pointers to 16-bit data
+                short* d16[PG_MAX_CHANNELS];
+                for(plaidgadget::Uint32 i = 0; i < chunk.format().channels; ++i)
                 {
-                    return output;
+                    d16[i] = (short*) chunk.start(i);
                 }
 
-                virtual bool exhausted()
+                while(true)
                 {
-                    return finished;
-                }
+                    int samples = ModPlug_Read(mod, &buffer[0], (need - have) * chunk.format().channels * 2) / chunk.format().channels / 2;
 
-                virtual void tick(Uint64 frame)
-                {
-                }
-
-                virtual void pull(AudioChunk& chunk)
-                {
-                    if(!chunk.length())
+                    if(samples < 0)
                     {
-                        return;
+                        finished = true;
+                        break;
                     }
-
-                    if(!valid || finished)
+                    if(samples == 0)
                     {
-                        chunk.silence();
-                        return;
-                    }
-
-                    int have = 0;
-                    int need = chunk.length();
-
-                    // Expand the intermediate buffer.
-                    if(need * chunk.format().channels > buffer.size())
-                    {
-                        buffer.resize(need * chunk.format().channels);
-                    }
-
-                    // Create pointers to 16-bit data
-                    short* d16[PG_MAX_CHANNELS];
-                    for(Uint32 i = 0; i < chunk.format().channels; ++i)
-                    {
-                        d16[i] = (short*) chunk.start(i);
-                    }
-
-                    while(true)
-                    {
-                        int samples = ModPlug_Read(mod, &buffer[0], (need - have) * chunk.format().channels * 2) / chunk.format().channels / 2;
-
-                        if(samples < 0)
+                        //File's end
+                        if(loop)
+                        {
+                            ModPlug_Seek(mod, 0);
+                            continue;
+                        }
+                        else
                         {
                             finished = true;
-                            //std::cout << " MODPLUG ERROR" << std::endl;
-                            break;
-                        }
-                        if(samples == 0)
-                        {
-                            //File's end
-                            if(loop)
-                            {
-                                ModPlug_Seek(mod, 0);
-                                continue;
-                            }
-                            else
-                            {
-                                finished = true;
-                                break;
-                            }
-                        }
-
-                        for(Uint32 i = 0; i < chunk.format().channels; ++i)
-                        {
-                            for(size_t j = 0; j < samples; ++j)
-                            {
-                                d16[i][j] = buffer[j * chunk.format().channels + i];
-                            }
-                            d16[i] += samples;
-                        }
-
-                        have += samples;
-                        //if (have > need) std::cout << "MODPLUG OVERDRAW" << std::endl;
-                        //std::cout << "MOD pull: " << have << "/" << need << std::endl;
-
-                        if(have >= need)
-                        {
                             break;
                         }
                     }
 
-                    //Cutoff marker if necessary
-                    if(have < need)
+                    for(plaidgadget::Uint32 i = 0; i < chunk.format().channels; ++i)
                     {
-                        chunk.cutoff(have);
-                    }
-
-                    //Upsample data to 24-bit Sint32s
-                    for (Uint32 i = 0; i < chunk.format().channels; ++i)
-                    {
-                        Sint32 *start = chunk.start(i), *op = start + have;
-                        short* ip = d16[i];
-                        while(op != start)
+                        for(size_t j = 0; j < samples; ++j)
                         {
-                            *(--op) = 256 * Sint32(*(--ip));
+                            d16[i][j] = buffer[j * chunk.format().channels + i];
                         }
+                        d16[i] += samples;
                     }
-                }
 
-            private:
-                bool loop, finished, valid;
-                AudioFormat output;
-                std::vector<uint16_t> buffer;
-                ModPlugFile* mod;
-        };
-
-
-        class Codec : public AudioCodec
-        {
-            public:
-                Codec()
-                    : AudioCodec(L"669,abc,amf,ams,dbm,dmf,dsm,far,it,j2b,mdl,med,mod,mt2,mtm,okt,pat,psm,ptm,s3m,stm,ult,umx,xm")
-                {
-                }
-
-                virtual ~Codec()
-                {
-                }
-
-                virtual Sound stream(const String& file, bool loop)
-                {
-                    ModplugStream* mod = new ModplugStream(file, loop);
-                    if (!mod->success())
+                    have += samples;
+                    if(have >= need)
                     {
-                        delete mod;
-                        return Sound();
+                        break;
                     }
-                    return Sound(mod);
                 }
-        };
 
-        // Register the codec by instantiating it in static time
-        static Codec codec;
-    }
+                //Cutoff marker if necessary
+                if(have < need)
+                {
+                    chunk.cutoff(have);
+                }
+
+                //Upsample data to 24-bit Sint32s
+                for(plaidgadget::Uint32 i = 0; i < chunk.format().channels; ++i)
+                {
+                    plaidgadget::Sint32* start = chunk.start(i);
+                    plaidgadget::Sint32* op = start + have;
+                    short* ip = d16[i];
+                    while(op != start)
+                    {
+                        *--op = 256 * plaidgadget::Sint32(*--ip);
+                    }
+                }
+            }
+
+        private:
+            bool loop, finished, valid;
+            plaidgadget::AudioFormat output;
+            std::vector<uint16_t> buffer;
+            ModPlugFile* mod;
+    };
+
+
+    class Codec : public plaidgadget::AudioCodec
+    {
+        public:
+            Codec()
+                : AudioCodec(L"669,abc,amf,ams,dbm,dmf,dsm,far,it,j2b,mdl,med,mod,mt2,mtm,okt,pat,psm,ptm,s3m,stm,ult,umx,xm")
+            {
+            }
+
+            virtual ~Codec()
+            {
+            }
+
+            virtual plaidgadget::Sound stream(const plaidgadget::String& file, bool loop)
+            {
+                ModplugStream* mod = new ModplugStream(file, loop);
+                if (!mod->success())
+                {
+                    delete mod;
+                    return plaidgadget::Sound();
+                }
+                return plaidgadget::Sound(mod);
+            }
+    };
+
+    // Register the codec by instantiating it in static time
+    static Codec codec;
 }
