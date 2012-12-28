92e929cbb77cc8d3ea9873daecc73021da9d65d6
Andrew G. Crowell <overkill9999@gmail.com>
2012-12-28 16:40:22 -0500

    Tabs to whitespace

--- test/Maximum Yak Maniac/button.lua
+++ test/Maximum Yak Maniac/button.lua
@@ -1,31 +1,31 @@
 do local Self = {}
-	function Button(...)
-		local self = setmetatable({}, {__index = Self})
-		self.unpress = false
-		self.inputs = { ... }
-		return self
-	end
+    function Button(...)
+        local self = setmetatable({}, {__index = Self})
+        self.unpress = false
+        self.inputs = { ... }
+        return self
+    end
 
-	function Self:held()
-		local pressed = false
-		for i, inp in ipairs(self.inputs) do
-			if inp.pressed then
-				pressed = true
-				break
-			end
-		end
-		
-		if not pressed then
-			if self.unpress then
-				self.unpress = false
-			end
-			return false
-		elseif not self.unpress then
-			return true
-		end
-	end
+    function Self:held()
+        local pressed = false
+        for i, inp in ipairs(self.inputs) do
+            if inp.pressed then
+                pressed = true
+                break
+            end
+        end
+        
+        if not pressed then
+            if self.unpress then
+                self.unpress = false
+            end
+            return false
+        elseif not self.unpress then
+            return true
+        end
+    end
 
-	function Self:release()
-		self.unpress = true
-	end
+    function Self:release()
+        self.unpress = true
+    end
 end
